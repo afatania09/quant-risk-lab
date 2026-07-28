b100cbcf75313bbd4f0e828d0c9ebbaafc607521
Ashutosh Fatania <124107322+afatania09@users.noreply.github.com>
2026-07-28 12:53:41 +0100

    Expand src/quant_risk/__init__.py

--- src/quant_risk/__init__.py
+++ src/quant_risk/__init__.py
@@ -1,23 +1,38 @@
 """Quant Risk Lab: transparent market, credit and counterparty risk models."""
 
 from .credit import expected_credit_loss, merton_default_probability
-from .cva import unilateral_cva
+from .credit_portfolio import credit_var_es, migrate_ratings, simulate_credit_losses
+from .cva import bilateral_cva_dva, collateralised_exposure, unilateral_cva
 from .market import (
     backtest_var,
+    component_parametric_var,
+    filtered_historical_var_es,
     historical_var_es,
     monte_carlo_var_es,
     parametric_var_es,
+    rolling_historical_var,
 )
 from .stress import apply_factor_shocks, historical_stress
+from .validation import christoffersen_independence_test, kupiec_test
 
 __all__ = [
     "apply_factor_shocks",
     "backtest_var",
+    "bilateral_cva_dva",
+    "christoffersen_independence_test",
+    "collateralised_exposure",
+    "component_parametric_var",
+    "credit_var_es",
     "expected_credit_loss",
+    "filtered_historical_var_es",
     "historical_stress",
     "historical_var_es",
+    "kupiec_test",
     "merton_default_probability",
+    "migrate_ratings",
     "monte_carlo_var_es",
     "parametric_var_es",
+    "rolling_historical_var",
+    "simulate_credit_losses",
     "unilateral_cva",
 ]
